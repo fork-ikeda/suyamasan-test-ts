5fb47b1f6f3f6a1bc8fe0c0592ea6167b27b4f6b
ikeda <ikeda@fork.co.jp>
2023-05-31 16:24:41 +0900

    ✨ commit fortune

--- src/Fortune.ts
+++ src/Fortune.ts
@@ -1,10 +1,40 @@
 export default class Fortune {
   private playerName: string
+  private playerBirthday: number
+  private luckyMode: boolean
 
-  constructor(name: string) {
+  constructor(name: string, birthday: number, mode: boolean) {
     this.playerName = name
+    this.playerBirthday = birthday
+    this.luckyMode = mode
   }
+
   public getPlayerName() {
     return this.playerName
+  }
+
+  public getPlayerBirthday() {
+    const year = this.playerBirthday.toString().substring(0, 4)
+    const month = this.playerBirthday.toString().substring(4, 6)
+    const date = this.playerBirthday.toString().substring(6)
+    return `${year}年${month}月${date}日`
+  }
+
+  public isLuckyMode() {
+    return this.luckyMode
+  }
+
+  public tellFortunes() {
+    const year = Number(this.playerBirthday.toString().substring(0, 4))
+    const month = Number(this.playerBirthday.toString().substring(4, 6)) - 1
+    const date = Number(this.playerBirthday.toString().substring(6))
+    const seconds = new Date(year, month, date).getTime() / 1000
+    const nameLength = this.playerName.length
+    const result = (seconds + nameLength) % 3
+    if(this.luckyMode) {
+      return result === 0 || result === 1 ? '良い': '普通'
+    } else {
+      return result === 0 ? '良い': result === 1 ? '普通' : '悪い'
+    }
   }
 }
--- tests/Fortune.test.ts
+++ tests/Fortune.test.ts
@@ -1,13 +1,82 @@
 import Fortune from '../src/Fortune'
 
-describe('Class Fortune', () => {
+describe('Class Fortune("ikeda", 19980518, false)', () => {
   let fortune: Fortune
   beforeEach(() => {
-    fortune = new Fortune("ikeda")
+    fortune = new Fortune("ikeda", 19980518, false)
   })
   describe("getPlayerName()", () => {
     it('returns string that is player name', () => {
       expect(fortune.getPlayerName()).toBe('ikeda')
+    })
+  })
+  describe("getPlayerBirthday()", () => {
+    it("returns string that is player birthday", () => {
+      expect(fortune.getPlayerBirthday()).toBe('1998年05月18日')
+    })
+  })
+  describe("isLuckyMode()", () => {
+    it('returns a boolean to determine the lucky mode', () => {
+      expect(fortune.isLuckyMode()).toBe(false)
+    })
+  })
+  describe('tellFortunes()', () => {
+    it("get the player's fortune results, which depending on the lucky mode", () => {
+      expect(fortune.tellFortunes()).toBe('悪い')
+    })
+  })
+})
+
+describe('Class Fortune("Schwarzenegger", 19470730, true)', () => {
+  let fortune: Fortune
+  beforeEach(() => {
+    fortune = new Fortune("Schwarzenegger", 19470730, true)
+  })
+  describe("getPlayerName()", () => {
+    it('returns string that is player name', () => {
+      expect(fortune.getPlayerName()).toBe('Schwarzenegger')
+    })
+  })
+  describe("getPlayerBirthday()", () => {
+    it("returns string that is player birthday", () => {
+      expect(fortune.getPlayerBirthday()).toBe('1947年07月30日')
+    })
+  })
+  describe("isLuckyMode()", () => {
+    it('returns a boolean to determine the lucky mode', () => {
+      expect(fortune.isLuckyMode()).toBe(true)
+    })
+  })
+  describe('tellFortunes()', () => {
+    it("get the player's fortune results, which depending on the lucky mode", () => {
+      expect(fortune.tellFortunes()).toBe('普通')
+    })
+  })
+})
+
+describe('Class Fortune("SnoopDogg", 19711020, false)', () => {
+  let fortune: Fortune
+  beforeEach(() => {
+    fortune = new Fortune("SnoopDogg", 19711020, false)
+  })
+  describe("getPlayerName()", () => {
+    it('returns string that is player name', () => {
+      expect(fortune.getPlayerName()).toBe('SnoopDogg')
+    })
+  })
+  describe("getPlayerBirthday()", () => {
+    it("returns string that is player birthday", () => {
+      expect(fortune.getPlayerBirthday()).toBe('1971年10月20日')
+    })
+  })
+  describe("isLuckyMode()", () => {
+    it('returns a boolean to determine the lucky mode', () => {
+      expect(fortune.isLuckyMode()).toBe(false)
+    })
+  })
+  describe('tellFortunes()', () => {
+    it("get the player's fortune results, which depending on the lucky mode", () => {
+      expect(fortune.tellFortunes()).toBe('良い')
     })
   })
 })
